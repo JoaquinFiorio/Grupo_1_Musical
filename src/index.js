92e8ce1d8ec1dfaa9026f25c2e6160c375cbbb65
JoaquinFiorio <97994656+JoaquinFiorio@users.noreply.github.com>
2023-10-19 12:43:39 -0300

    update modelo vista controlador

--- src/index.js
+++ src/index.js
@@ -2,77 +2,22 @@ const express = require('express');
 const path = require('path');
 require("dotenv").config();
 
+/* REQUERIMOS LAS RUTAS DEL ARCHIVO */
+
+const vistaRutas = require("./rutas/ruta");
+
+/* INICIALIZAMOS TODOS LOS METODOS DE EXPRESS */
 const app = express();
 
+/* MOTOR DE PLANTILLA */
 app.set('view engine', 'ejs');
+
+/* ARCHIVOS ACCESIBLES PARA TODOS, RUTA ABSOLUTA */
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-    const data = {
-        title: 'Home',
-        css: 'index.css'
-    };
-    res.render(path.join(__dirname, 'views/index'), data);
-});
-
-app.get('/login', (req, res) => {
-    const data = {
-        title: 'Login',
-        css: 'login.css'
-    };
-    res.render(path.join(__dirname, 'views/login'), data);
-});
-
-app.get('/productCart', (req, res) => {
-    const data = {
-        title: 'Product Cart',
-        css: 'productCart.css'
-    };
-    res.render(path.join(__dirname, 'views/productCart'), data);
-});
-
-app.get('/productDetail', (req, res) => {
-    const data = {
-        title: 'Product Detail',
-        css: 'productDetail.css'
-    };
-    res.render(path.join(__dirname, 'views/productDetail'), data);
-});
-
-app.get('/register', (req, res) => {
-    const data = {
-        title: 'Register',
-        css: 'register.css'
-    };
-    res.render(path.join(__dirname, 'views/register'), data);
-});
-
-app.get('/productList', (req, res) => {
-    const data = {
-        title: 'Product List',
-        css: 'productList.css'
-    };
-    res.render(path.join(__dirname, 'views/productList'), data);
-});
-
-app.get('/productUploader', (req, res) => {
-    const data = {
-        title: 'Product Uploader',
-        css: 'productUploader.css'
-    };
-    res.render(path.join(__dirname, 'views/productUploader'), data);
-});
-
-app.get('/productEdit', (req, res) => {
-    const data = {
-        title: 'Product Edit',
-        css: 'productEdit.css'
-    };
-    res.render(path.join(__dirname, 'views/productEdit'), data);
-});
-
+/* RUTAS */
+app.use("/", vistaRutas);
 
 app.listen(process.env.PORT || 3030, () => {
     console.log(`Servidor iniciado en http://localhost:${process.env.PORT}`);
 });
-
