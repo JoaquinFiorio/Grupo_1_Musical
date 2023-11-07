a29419a244dab9eaecb71bc49d2b06190e1b360a
ImaMartinez <imanolmartinez249@gmail.com>
2023-11-07 14:47:26 -0300

    update delete product

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const path = require('path');
 require("dotenv").config();
+const methodOverride = require('method-override');
 
 /* REQUERIMOS LAS RUTAS DEL ARCHIVO */
 
-const vistaRutas = require("./rutas/ruta");
-const productRoutes = require("./rutas/rutaProductos")
+const viewsRoutes = require("./routes/main");
+const productRoutes = require("./routes/products");
 
 /* INICIALIZAMOS TODOS LOS METODOS DE EXPRESS */
 const app = express();
@@ -16,9 +17,10 @@ app.set('view engine', 'ejs');
 /* ARCHIVOS ACCESIBLES PARA TODOS, RUTA ABSOLUTA */
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json())
+app.use(methodOverride('_method'));
 
 /* RUTAS */
-app.use("/", vistaRutas);
+app.use("/", viewsRoutes);
 app.use("/products", productRoutes);
 
 app.listen(process.env.PORT || 3030, () => {
