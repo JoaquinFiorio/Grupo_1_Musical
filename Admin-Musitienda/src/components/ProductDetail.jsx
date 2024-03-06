5de5854ad6cd340cfbb4f7a98580c6fe17976353
ImaMartinez <imanolmartinez249@gmail.com>
2024-03-06 10:47:54 -0300

    correccion de errores

--- Admin-Musitienda/src/components/ProductDetail.jsx
+++ Admin-Musitienda/src/components/ProductDetail.jsx
@@ -1,5 +1,5 @@
 // ProductDetail.jsx
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { useParams, Link } from "react-router-dom";
 
 const ProductDetail = () => {
